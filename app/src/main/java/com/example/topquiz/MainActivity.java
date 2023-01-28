f596c88cd2cd97363eb7ea40e372b08896df0f75
Axel O <olougounaaxel@gmail.com>
2023-01-28 22:28:33 +0100

    xml

--- app/src/main/java/com/example/topquiz/MainActivity.java
+++ app/src/main/java/com/example/topquiz/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.topquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mGreetingTextView;
+    private EditText mNameEditText;
+    private Button mPlayButton;
+
+    mGreetingTextView = findViewById(R.id.main_textview_greeting);
+    mNameEditText = findViewById(R.id.main_edittext_name);
+    mPlayButton = findViewById(R.id.main_button_play);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mPlayButton.setEnabled(false);
     }
+
 }
